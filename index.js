98d648cb9c0369b52a5e2ee66e95aca65f0f8c91
TamaoMiura <miuratamao.d@gmail.com>
2024-05-16 13:28:55 +0900

    - refactor

--- index.js
+++ index.js
@@ -75,8 +75,15 @@ app.patch("/jokes/:id", (req, res) => {
 app.delete("/jokes/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const searchIndex = jokes.findIndex((joke) => { joke.id === id });
-  jokes.splice(searchIndex, 1)
-  res.json("OK");
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.json("OK");
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({ error: `Joke with id: ${id} not found. No jokes were deleted.`})
+  }
 })
 
 //8. DELETE All jokes
